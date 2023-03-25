e9a34d971bf9e1409b09ba2db5539529b8e83271
Fitzy <fitzystrikesagain@gmail.com>
2023-03-25 03:53:43 -0500

    add func docstrings to test output

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+def pytest_itemcollected(item):
+    """
+    Adds a test function's docstring to the test output, e.g.,
+    module True must be True <- tests/sample_test.py PASSED  [ 11%]
+    """
+    par = item.parent.obj
+    node = item.obj
+    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
+    suf = node.__doc__.strip() if node.__doc__ else node.__name__
+    if pref or suf:
+        item._nodeid = " ".join((pref, suf))
