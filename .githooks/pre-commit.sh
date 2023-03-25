7f3f42eaaab198158fa950b74b03c172ae1c8d89
Fitzy <fitzystrikesagain@gmail.com>
2023-03-25 14:02:20 -0500

    only check changed files

--- .githooks/pre-commit.sh
+++ .githooks/pre-commit.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
-# ^ Note the above "shebang" line. This says "This is an executable shell script"
-# Name this script "pre-commit" and place it in the ".git/hooks/" directory
 
 # If any command fails, exit immediately with that command's exit status
 set -eo pipefail
 
-# Run flake8 against all code in the `source_code` directory
-flake8
-echo "flake8 passed!"
+# Find all changed files for this commit
+# Compute the diff only once to save a small amount of time.
+CHANGED_FILES=$(git diff --name-only --cached --diff-filter=ACMR)
+# Get only changed files that match our file suffix pattern
+get_pattern_files() {
+    pattern=$(echo "$*" | sed "s/ /\$\\\|/g")
+    echo "$CHANGED_FILES" | { grep "$pattern$" || true; }
+}
+# Get all changed python files
+PY_FILES=$(get_pattern_files .py)
 
-# Run black against all code in the `source_code` directory
-black
-echo "black passed!"
+if [[ -n "$PY_FILES" ]]
+then
+    # Run black against changed python files for this commit
+    black --check $PY_FILES
+    echo "black passes all altered python sources."
+    # Run flake8 against changed python files for this commit
+    flake8 $PY_FILES
+    echo "flake8 passed!"
+fi
